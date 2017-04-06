62f31114216d81c06fec9d3513b23674c8932f23
Vadim Kochan <vadim4j@gmail.com>
2017-04-06 06:47:38 +0300

    log: Fix error(...) log level
    Change LOG_INFO -> LOG_ERR level in error(...) printing.

--- T1/src/log.h
+++ T1/src/log.h
@@ -15,7 +15,7 @@ void log_uninit(void);
 int log_printf(int level, const char *fmt, ...);
 
 #define info( ...) log_printf(LOG_INFO, __VA_ARGS__)
-#define error( ...) log_printf(LOG_INFO, __VA_ARGS__)
+#define error( ...) log_printf(LOG_ERR, __VA_ARGS__)
 #define warn( ...) log_printf(LOG_WARNING, __VA_ARGS__)
 #define alert( ...) log_printf(LOG_ALERT, __VA_ARGS__)
 
